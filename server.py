ee74db0276dffb379b1fb540924217444afc6779
Fares-123 <77728948+Fares-123@users.noreply.github.com>
2025-01-02 02:14:55 +0300

    Server.py

--- server.py
+++ server.py
@@ -13,8 +13,8 @@ TEMP_FOLDER = "temp_images"
 os.makedirs(TEMP_FOLDER, exist_ok=True)
 
 
-GITHUB_TOKEN = "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"  # ضع GitHub Token هنا
-github = Github(GITHUB_TOKEN)  # استخدم التوكن هنا
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')  # استدعاء التوكن من المتغير البيئي
+github = Github(GITHUB_TOKEN)
 repo = github.get_repo("Fares-123/Manga.translator")  # اسم المستودع
 
 
