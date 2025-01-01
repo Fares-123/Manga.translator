bae8547dd0ad65732816e7809d4be826273237ad
Fares-123 <77728948+Fares-123@users.noreply.github.com>
2025-01-02 01:11:25 +0300

    Server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ translator = Translator()
 TEMP_FOLDER = "temp_images"
 os.makedirs(TEMP_FOLDER, exist_ok=True)
 
-GITHUB_TOKEN = "your_github_token"  # ضع GitHub Token هنا
+GITHUB_TOKEN = "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"  # ضع GitHub Token هنا
 REPO_NAME = "username/translated-chapters"  # اسم المستودع
 github = Github(GITHUB_TOKEN)
 repo = github.get_repo(REPO_NAME)
